909ce8635aa6b5bb8ebe7739b80489a5a9a2b37e
Kamil Ziętek <kamil.zietek@allinmobile.co>
2018-07-13 17:08:40 +0200

    POR-9 Refactor

--- lib/AhoyCell.dart
+++ lib/AhoyCell.dart
@@ -27,20 +27,50 @@ class AhoyCellData {
 class AhoyCell extends StatelessWidget {
   final AhoyCellData data;
 
-  AhoyCell(this.data)
+  AhoyCell(this.data);
 
   @override Widget build(BuildContext context) {
-    final double scale = 2.0;
-    final accentColor = AhoyStyles.accentColor;
-    final grey = AhoyStyles.grey;
-    final dark = AhoyStyles.dark;
-    final baseTextStyle = AhoyStyles.baseTextStyle;
-    final titleStyle = AhoyStyles.titleStyle;
-    final subtitleStyle = AhoyStyles.subtitleStyle;
-    final captionStyle = AhoyStyles.captionStyle;
-    final valueStyle = AhoyStyles.valueStyle;
-    final accentedValueStyle = AhoyStyles.accentedValueStyle;
+    return _background(
+      Stack(children: <Widget>[
+        _icon(data.iconName),
+        _topRightText(data.topRightText),
+        Column(children: <Widget>[
+          _title(data.title),
+          _subtitle(data.subtitle),
+          Row(children: <Widget>[
+            _bottomText(data.bottomLeftCaption, data.bottomLeftValue, false),
+            _flexibleSpace(),
+            _bottomText(data.bottomRightCaption, data.bottomRightValue, true),
+          ],),
+        ],),
+      ],),      
+    );
+  }
 
+  _bottomText(String caption, String value, bool accented) {
+    final captionStyle = AhoyStyles.captionStyle;
+    final style = accented == true ? AhoyStyles.accentedValueStyle : AhoyStyles.valueStyle;
+    return Row(children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(0.0, 0.0, 4.0, 0.0),
+        child: Text(caption, style: captionStyle,),
+      ),
+      Text(value, style: style,),
+    ],);
+  }
+
+  _topRightText(String text) {
+    final valueStyle = AhoyStyles.valueStyle;
+    return Container(alignment: Alignment.topRight,
+      child:Text(text, style: valueStyle,),
+    );
+  }
+
+  Widget _flexibleSpace() {
+    return Expanded(child:Container());
+  }
+
+  Widget _background(Widget child) {
     return Container(
       // height: 55.5*2,
       margin: const EdgeInsets.symmetric(
@@ -59,58 +89,31 @@ class AhoyCell extends StatelessWidget {
           ),
         ],
       ),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            alignment: FractionalOffset.topLeft,
-            child:new Image(
-              image: new AssetImage(data.iconName),
-              height: 24.0,
-              width: 24.0,
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              Text(data.title.toUpperCase(), style: titleStyle,),
-              Container(
-                margin: EdgeInsets.only(top: 7.0, bottom: 16.0,),
-                child: Text(data.subtitle, style: subtitleStyle,),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0.0, 0.0, 4.0, 0.0),
-                      child: Text(data.bottomLeftCaption, style: captionStyle,),
-                    ),
-                    Text(data.bottomLeftValue, style: valueStyle,),
-                  ],),
-                  Row(children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0.0, 0.0, 4.0, 0.0),
-                      child: Text(data.bottomRightCaption, style: captionStyle,),
-                    ),
-                    Text(data.bottomRightValue, style: accentedValueStyle,),
-                  ],)
-                ],
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text(data.topRightText, style: valueStyle,),
-                ],)
-              )
-            ],
-          )
-        ],
-      ),      
-      );
+      child: child
+    );
+  }
+
+  Widget _icon(String imageName) {
+    return Container(
+      alignment: FractionalOffset.topLeft,
+      child:new Image(
+        image: new AssetImage(imageName),
+        height: 24.0,
+        width: 24.0,
+      ),
+    );
+  }
+  
+  Widget _title(String text) {
+    final titleStyle = AhoyStyles.titleStyle;
+    return Text(text.toUpperCase(), style: titleStyle,);
+  }
+
+  Widget _subtitle(String text) {
+    final subtitleStyle = AhoyStyles.subtitleStyle;
+    return Container(
+      margin: EdgeInsets.only(top: 7.0, bottom: 16.0,),
+      child: Text(text, style: subtitleStyle,),
+    );
   }
 }
-
